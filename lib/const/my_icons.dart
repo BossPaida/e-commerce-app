d0992ee42c45bd374846096cae9b61fef5290913
BossPaida <paidachikumbu@gmail.com>
2021-10-21 12:48:10 +0200

    icon class

---
+++ lib/const/my_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_icons/flutter_icons.dart';
+
+class MyIcons {
+  static IconData home = Feather.home;
+  static IconData rss = Feather.rss;
+  static IconData search = Feather.search;
+  static IconData user = Feather.user;
+  static IconData cart = MaterialCommunityIcons.cart;
+}
